7a23f07b1dd0211312de411187f6a544f7f0f4fc
vumanhdung <dungvmph04939@fpt.edu.vn>
2018-08-02 10:45:06 +0700

    Initial commit

---
+++ app/src/main/java/com/example/cyber/duan1/giaodien.java
@@ -0,0 +1,13 @@
+package com.example.cyber.duan1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class giaodien extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_giaodien);
+    }
+}
